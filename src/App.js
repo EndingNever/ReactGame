058442f3eb7df47248f932e22399183823965cce
Fabian Villasenor <exionite@gmail.com>
2022-05-03 16:17:30 -0700

    shuffle

--- src/App.js
+++ src/App.js
@@ -4,48 +4,45 @@ import Deck from './Components/Deck/Deck';
 import Die from './Components/Die/Die';
 
 function App() {
-  const suits = ["spades", "diamonds", "clubs", "hearts"];
-  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-  const player1Hand = [];
-  const player2Hand = [];
+  // const suits = ["spades", "diamonds", "clubs", "hearts"];
+  // const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  // const player1Hand = [];
+  // const player2Hand = [];
 
-  const deck = new Array();
-  // const [stateDeck, setStateDeck] = useState([])
+  // const deck = new Array();
+  // // const [stateDeck, setStateDeck] = useState([])
 
-  const getDeck = () => {
-    for (let i = 0; i < suits.length; i++) {
-      for (let x = 0; x < values.length; x++) {
-        let card = { value: values[x], suit: suits[i] };
-        deck.push(card);
-      }
-    }
-    // setStateDeck(oldArray => [...oldArray, deck])
-  }
+  // const getDeck = () => {
+  //   for (let i = 0; i < suits.length; i++) {
+  //     for (let x = 0; x < values.length; x++) {
+  //       let card = { value: values[x], suit: suits[i] };
+  //       deck.push(card);
+  //     }
+  //   }
+  //   // setStateDeck(oldArray => [...oldArray, deck])
+  // }
 
-  const shuffleDeck = () => {
-    for (let i = 0; i < 1000; i++) {
-      let location1 = Math.floor((Math.random() * deck.length));
-      let location2 = Math.floor((Math.random() * deck.length));
-      let tmp = deck[location1];
+  // const shuffleDeck = () => {
+  //   for (let i = 0; i < 1000; i++) {
+  //     let location1 = Math.floor((Math.random() * deck.length));
+  //     let location2 = Math.floor((Math.random() * deck.length));
+  //     let tmp = deck[location1];
 
-      deck[location1] = deck[location2];
-      deck[location2] = tmp;
-    }
-  }
+  //     deck[location1] = deck[location2];
+  //     deck[location2] = tmp;
+  //   }
+  // }
 
   // useEffect(()=>{
   //   getDeck();
   //   shuffleDeck();
   //   console.log(stateDeck)
   // }, [])
-  useEffect(() => {
-    getDeck();
-    shuffleDeck();
-  }, [])
+
 
   return (
     <div className="App">
-      <Deck deck={deck}/>
+      <Deck />
     </div>
   );
 }
