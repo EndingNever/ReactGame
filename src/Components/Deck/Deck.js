a10735dabca5e4a1510285faa64ec1d5b6d5e983
Fabian Villasenor <exionite@gmail.com>
2022-05-05 15:54:38 -0700

    NESW Deck

--- src/Components/Deck/Deck.js
+++ src/Components/Deck/Deck.js
@@ -6,6 +6,10 @@ import "./Deck.scss"
 const Deck = (props) => {
   const deck = props.deck;
   const [setupCards, setSetupCards] = useState([]);
+  const [NDeck, setNDeck] = useState([]);
+  const [EDeck, setEDeck] = useState([]);
+  const [SDeck, setSDeck] = useState([]);
+  const [WDeck, setWDeck] = useState([]);
   const [player1Hand, setPlayer1Hand] = useState([]);
   const [player2Hand, setPlayer2Hand] = useState([]);
   const [playerSelectedCard, setPlayerSelectedCard] = useState([]);
@@ -33,24 +37,79 @@ const Deck = (props) => {
     dealPlayerHand(deck); // Calls the dealPlayerHand function and passes through props.deck
   }
 
-  const dealSetupCards = () => {
-    if (setupCards.length < 4) { // Checks to see if the array of setupCards has less than 4, otherwise the setup cards have already been dealt
-      for (let i = 0; i < 4; i++) {
-        const shift = deck.shift(); // Draws 4 cards from the shuffled deck
-        setSetupCards(oldArray => [...oldArray, shift]); // pushes the cards to the setupCards array
-      }
+  //TODO translate to 4arrs
+  // const dealSetupCards = () => {
+  //   if (setupCards.length < 4) { // Checks to see if the array of setupCards has less than 4, otherwise the setup cards have already been dealt
+  //     for (let i = 0; i < 4; i++) {
+  //       const shift = deck.shift(); // Draws 4 cards from the shuffled deck
+  //       setSetupCards(oldArray => [...oldArray, shift]); // pushes the cards to the setupCards array
+  //     }
+  //   } else {
+  //     console.log("setup cards are out") // If setupCards.length > 4, that means cards have already been setup
+  //   }
+  // }
+
+  const dealNDeck = () => {
+    if (NDeck.length < 1) {
+    const shift = deck.shift();
+      setNDeck(oldArray => [...oldArray, shift]);
     } else {
-      console.log("setup cards are out") // If setupCards.length > 4, that means cards have already been setup
+      console.log("Setup is done")
     }
   }
+  const dealWDeck = () => {
+    if (WDeck.length < 1) {
+    const shift = deck.shift();
+      setWDeck(oldArray => [...oldArray, shift]);
+    } else {
+      console.log("Setup is done")
+    }
+  }
+
+  const dealEDeck = () => {
+    if (EDeck.length < 1) {
+    const shift = deck.shift();
+      setEDeck(oldArray => [...oldArray, shift]);
+    } else {
+      console.log("Setup is done")
+    }
+  }
+
+  const dealSDeck = () => {
+    if (SDeck.length < 1) {
+      const shift = deck.shift();
+      setSDeck(oldArray => [...oldArray, shift]);
+    } else {
+      console.log("Setup is done")
+    }
+  }
+
+  const dealFourArrs = () => {
+    dealNDeck();
+    dealWDeck();
+    dealEDeck();
+    dealSDeck();
+    console.log(NDeck, EDeck, WDeck, SDeck)
+  }
+
+  //TODO Following function is based on dealSetupCards above, may remove 
+  // const setDecks = (arr) => { 
+  //   if (setupCards.length === 0) {
+  //     dealSetupCards();
+  //   }
+  //   if (arr.length < 1) {
+  //     const shift = deck.shift();
+  //     arr(oldArray => [...oldArray, shift]);
+  //   }
+  // }
 
   return (
     <div className='deck-container'>
       <button onClick={() => console.log(deck.length)}>Check Deck</button>
       <button onClick={dealPlayers}>Deal Players</button>
-      <button onClick={dealSetupCards}>Deal Setup</button>
-      <PlayField playerSelectedCard={playerSelectedCard} setupCards={setupCards} />
-      <PlayerCards {...{player1Hand, player2Hand, playerSelectedCard, setPlayerSelectedCard}} />
+      <button onClick={dealFourArrs}>Deal Four Arrs</button>
+      <PlayField playerSelectedCard={playerSelectedCard} NDeck={NDeck} EDeck={EDeck} SDeck={SDeck} WDeck={WDeck} />
+      <PlayerCards {...{ player1Hand, player2Hand, playerSelectedCard, setPlayerSelectedCard }} />
     </div>
   );
 }
--- src/Components/PlayField/PlayField.js
+++ src/Components/PlayField/PlayField.js
@@ -2,12 +2,37 @@ import React from 'react'
 import "./PlayField.scss"
 
 export default function PlayField(props) {
-  const setupCards = props.setupCards;
-  const playerSelectedCard = props.playerSelectedCard
+  const NDeck = props.NDeck;
+  const EDeck = props.EDeck;
+  const SDeck = props.SDeck;
+  const WDeck = props.WDeck;
 
+  const playerSelectedCard = props.playerSelectedCard
+  console.log(NDeck)
   return (
     <div className='playfield-container'>
-      {setupCards.map((card, index) => (
+      {NDeck.map((card, index) => (
+        <div key={index} className="starter-card card-0">
+          {card.value} {card.suit}
+        </div>
+      ))}
+      {EDeck.map((card, index) => (
+        <div key={index} className="starter-card card-1">
+          {card.value} {card.suit}
+        </div>
+      ))}
+      {SDeck.map((card, index) => (
+        <div key={index} className="starter-card card-2">
+          {card.value} {card.suit}
+        </div>
+      ))}
+      {WDeck.map((card, index) => (
+        <div key={index} className="starter-card card-3">
+          {card.value} {card.suit}
+        </div>
+      ))}
+
+      {/* {setupCards.map((card, index) => (
         <>
           <div value={card.value} className={`starter-card card-${index}`} >{card.value}</div>
         </>
@@ -19,7 +44,7 @@ export default function PlayField(props) {
           <div className="setup-spot starter-card card-2">2</div>
           <div className="setup-spot starter-card card-3">3</div>
         </>
-      }
+      } */}
       <div className="deck">Deck</div>
       <div className="starter-card card-king king-0">King goes here</div>
       <div className="starter-card card-king king-1">King goes here</div>
